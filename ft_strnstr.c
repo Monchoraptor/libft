1e4a0548f5aaa204635cee742b393549b9964ddf
Monchoraptor <amoracho98@gmail.com>
2020-07-11 18:53:57 +0200

    fix 01 strnstr?

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,12 +6,13 @@
 /*   By: amoracho <amoracho@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/10 17:56:53 by amoracho          #+#    #+#             */
-/*   Updated: 2020/07/11 01:36:11 by amoracho                   ###   ########.fr       */
+/*   Updated: 2020/07/11 18:53:20 by amoracho                   ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "libft.h"
+#include <string.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
@@ -22,7 +23,9 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	le = 0;
 	i = 0;
 	j = 0;
-	while (i < len)
+	if (*needle == '\0')
+		return ((char *)haystack);
+	while (i < len + 1)
 	{
 		if (j == 0)
 			le = (char *)haystack + i;
@@ -37,10 +40,11 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	return (0);
 }
 /*
-**int main(int argc, char **argv)
-**{
-**	(void)argc;
-**	printf("%s",ft_strnstr(argv[1],argv[2],ft_strlen(argv[1])));
-**	return 0;
-**}
+**	int main(int argc, char **argv)
+**	{
+**		(void)argc;
+**		printf("Mia----->%s\n",ft_strnstr(argv[1],argv[2],5));
+**		printf("Otra====>%s",strstr(argv[1],argv[2]));
+**		return 0;
+**	}
 */
