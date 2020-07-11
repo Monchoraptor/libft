9bcf69b8c38ac0baa6ae36774e389d062e854b00
Monchoraptor <amoracho98@gmail.com>
2020-07-11 17:44:25 +0200

    fix 02 strlcat?

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,22 +6,24 @@
 /*   By: amoracho <amoracho@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 16:12:58 by amoracho          #+#    #+#             */
-/*   Updated: 2020/07/11 17:20:59 by amoracho                   ###   ########.fr       */
+/*   Updated: 2020/07/11 17:44:12 by amoracho                   ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <string.h>
 #include "libft.h"
+#include <bsd/string.h>
 
 size_t	ft_strlcat(char *dst, char *src, size_t size)
 {
 	size_t i;
 	size_t j;
+	size_t k;
 
 	if (size > 0 && size - 1 > ft_strlen(dst))
 	 {
 		i = ft_strlen(dst);
+		k = ft_strlen(dst);
 		j = 0;
 		while (*(src + j) && i < size - 1)
 		{
@@ -30,21 +32,20 @@ size_t	ft_strlcat(char *dst, char *src, size_t size)
 			j++;
 		}
 		*(dst + i) = '\0';
-		return (ft_strlen(src) + ft_strlen(dst));
+		return (ft_strlen(src) + k);
 	}
 	return (size + ft_strlen(src));
 }
-
 /*
 **int main(int argc, char **argv)
 **{
 **	if (argc<argc+1)
 **	{
 **	char abc[100];
-**	strlcpy(abc,argv[1],20);
+**	ft_strlcpy(abc,argv[1],20);
 **	char abc2[100];
-**	strlcpy(abc2,argv[1],20);
+**	ft_strlcpy(abc2,argv[1],20);
 **	printf("\n%lu\n",ft_strlcat(abc, argv[2],argv[3][0]-48));
-**	printf("\n%lu\n",strlcat(abc2, argv[2],argv[3][0]-48));
+** //	printf("\n%lu\n",strlcat(abc2, argv[2],argv[3][0]-48));
 **	printf("%s\n%s",abc,abc2);}}
 */
