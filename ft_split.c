56651a77de8f6ec27cdf3aaaebe2a1d3392a5f6f
Monchoraptor <amoracho98@gmail.com>
2020-07-12 22:28:51 +0200

    fix 01 split?

--- ft_split.c
+++ ft_split.c
@@ -13,19 +13,15 @@
 #include "libft.h"
 #include <stdio.h>
 
-int	ft_countuntil(char const *s, char c, int n)
+int	ft_countuntil(char const *s, char c)
 {
 	int i;
-	int n1;
 
 	i = 0;
-	n1 = 0;
 	while (*(s + i) != 0)
 	{
-		if (*(s + i) == c && n1 == n)
+		if (*(s + i) == c )
 			return(i);
-		else if (*(s + i) == c)
-			n1++;
 		i++;
 	}
 	return (i);
@@ -44,8 +40,20 @@ int	ft_countocurrences(char const *s, char c)
 			i++;
 		j++;
 	}
-	
 	return (i);
+}
+
+char	*ft_chartostr(char c)
+{
+	char	*s;
+
+	s = malloc(sizeof(char) * 2);
+	if (s)
+	{
+		s[0] = c;
+		s[1] = '\0';
+	}
+	return (s);
 }
 
 char	**ft_split(char const *s, char c)
@@ -54,40 +62,40 @@ char	**ft_split(char const *s, char c)
 	char	*aux;
 	char	**ar;
 	int	j;
-	int	k;
 
 	ar = malloc(sizeof(char *) * (ft_countocurrences(s, c) + 1));
 	if (!ar)
 		return (0);
 	i = 0;
-	k = 0;
+	j = 0;
 	while (i < ft_countocurrences(s, c) + 1)
 	{
-		j = 0;
-		aux = malloc(sizeof(char) * (ft_countuntil(s, c, i) + 1));
-		if (!s)
+		aux = ft_substr(s, j, ft_countuntil(s + j, c));
+		j = ft_strnstr(s + j, ft_chartostr(c),  ft_strlen(s + j)) - s + 1;
+		if (!aux)
 			return (0);
-		while (k < ft_countuntil(s, c, i) + 1)
-			*(aux + j++) = *(s + k++);
-		*(aux + j - 1) = '\0';
-		*(ar + i) = aux;
-		i++;
+		ar[i++] = aux;
 	}
 	return (ar);
 }
 /*
+**
 **int main(int argc, char **argv)
 **{
 **	char s[50]="aaaXXaaXa";
-**	//printf("a\n");
+** //	printf("a\n");
 **	char **p = ft_split(argv[1], argv[2][0]);
-**	//printf("d\n");
+** //	printf("d\n");
 **	int i = 0;
-**	while(i < ft_countocurrences(argv[1], argv[2][0]) + 1)
+**	if (p)
 **	{
-**		printf("%s\n",p[i]);
-**		//printf("%i|%c\n",p[0][i],p[0][i]);
-**		i -= -1;
+**		while(i < 4)
+**		{
+**			if(p[i])
+**				printf("%s\n",p[i]);
+**			//printf("%i|%c\n",p[0][i],p[0][i]);
+**			i -= -1;
+**		}
 **	}
 **	return (0);
 **}
