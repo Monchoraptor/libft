b7cbb3dda04cd8cd9c1e37d457cf7d8e70004042
Monchoraptor <amoracho98@gmail.com>
2020-07-12 18:49:59 +0200

    fix 01 strtrim?

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: amoracho <amoracho@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 17:06:07 by amoracho          #+#    #+#             */
-/*   Updated: 2020/07/11 04:52:45 by amoracho                   ###   ########.fr       */
+/*   Updated: 2020/07/12 15:20:53 by amoracho                   ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,42 @@
 
 char		*ft_strtrim(char const *s1, char const *set)
 {
-	int	i;
-	int	j;
-	int	k;
-	char	*aux;
+    char	*aux;
+    size_t	i;
+    size_t	j;
+    size_t	k;
 
-	j = ft_strlen(s1);
-	aux = malloc(j);
-	if (!aux)
-		return (0);
-	i = 0;
-	while (ft_isin(set, *(s1 + i)))
-		i++;
-	while (ft_isin(set, *(s1 + j - 1)))
-		j--;
-	k = 0;
-	while (i < j)
-	{
-		*(aux + k) = *(s1 + i);
-		i++;
-		k++;
-	}
-	*(aux + k + 1) = '\0';
-	return (aux);
+    j = 0;
+    k = ft_strlen(s1);
+    while (s1[j] && ft_isin(set, s1[j]))
+    	j++;
+    while (k > j && ft_isin(set, s1[k - 1]))
+        k--;
+    aux = (char*)malloc(sizeof(*s1) * (k - j + 1));
+    if (!aux)
+        return (NULL);
+    i = 0;
+    while (j < k)
+        aux[i++] = s1[j++];
+    aux[i] = 0;
+    return (aux);
 }
 /*
-**int main(int argc, char **argv)
-**{
-**	char *a = malloc(100);
-**	ft_strlcpy(a, argv[1], ft_strlen(argv[1]));
-** //	char *b = malloc(100);
-** //	if (b == 0) 
-** //	ft_strlcpy(b, argv[2], ft_strlen(argv[2]));
-**	char *b = "t";
-** //	printf("%s\n",a);
-** //	printf("%s\n",b);
-**	char *s1 = ""; 
-**	char *s2 = "a";
-**	printf("%s",ft_strtrim(s1,s2));
-**		//printf("%c/%i/%s\n",*(s1 + i),!ft_isin(set, *(s1 + i)),c);
-**}
+
+	int main(int argc, char **argv)
+	{
+	char *a = malloc(100);
+	ft_strlcpy(a, argv[1], ft_strlen(argv[1]));
+ //	char *b = malloc(100);
+ //	if (b == 0)
+ //	ft_strlcpy(b, argv[2], ft_strlen(argv[2]));
+	char *b = "t";
+ //	printf("%s\n",a);
+ //	printf("%s\n",b);
+	char *s1 = "";
+	char *s2 = "a";
+	printf("-->%s<--",ft_strtrim(s1,s2));
+		//printf("%c/%i/%s\n",*(s1 + i),!ft_isin(set, *(s1 + i)),c);
+	}
+
 */
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: amoracho <amoracho@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 17:04:29 by amoracho          #+#    #+#             */
-/*   Updated: 2020/07/11 19:29:55 by amoracho                   ###   ########.fr       */
+/*   Updated: 2020/07/11 19:32:05 by amoracho                   ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*p;
 
 	p = malloc(len + 1);
-	if (start >= ft_strlen(s))
+	if (start >= ft_strlen(s) && p)
 	{
 		*(p) = '\0';
 		return (p);
