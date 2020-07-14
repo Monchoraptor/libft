8eeea934eb85014ed51b224c9c77cf466501eac1
Monchoraptor <amoracho98@gmail.com>
2020-07-14 16:02:32 +0200

    fix substr 01?

--- ft_substr.c
+++ ft_substr.c
@@ -17,39 +17,39 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	size_t	i;
-	size_t	y;
-	char	*p;
+	{
+		size_t	i;
+		size_t	j;
+		char	*aux;
 
-	p = malloc(len + 1);
-	if (start >= ft_strlen(s) && p)
-	{
-		*(p) = '\0';
-		return (p);
+		aux = (char*)malloc(sizeof(*s) * (len + 1));
+		if (!aux)
+			return (NULL);
+		i = 0;
+		j = 0;
+		while (s[i])
+		{
+			if (i >= start && j < len)
+			{
+				aux[j] = s[i];
+				j++;
+			}
+			i++;
+		}
+		aux[j] = 0;
+		return (aux);
 	}
-	if (p == 0 || (len + start) > ft_strlen(s) || start > ft_strlen(s))
-		return (0);
-	i = 0;
-	y = start;
-	while ((i < len) && (s[y] != 0))
-	{
-		*(p + i) = s[y];
-		i++;
-		y++;
-	}
-	*(p + i) = '\0';
-	return (p);
 }
 /*
-**int main(int argc, char **argv)
-**{
-** //	char a[10] = "qwerty";
-** //	char b[10] = "";
-**	char *s;
-**	s = ft_substr(argv[1],argv[2][0]-'0',10);
-**	if (s != 0)
-**		printf("%s",s);
-**	printf("\n%s",ft_substr(argv[1],argv[2][0]-'0',10));
-** //argv[3][0]-'0'));
-**}
+**	int main(int argc, char **argv)
+**	{
+**	 //	char a[10] = "qwerty";
+**	 //	char b[10] = "";
+**		char *s;
+**		s = ft_substr(argv[1],argv[2][0]-'0',argv[3][0]-'0');
+**		if (s != 0)
+**			printf("%s",s);
+**		//printf("\n%s",ft_substr(argv[1],argv[2][0]-'0',10));
+**	 //argv[3][0]-'0'));
+**	}
 */
