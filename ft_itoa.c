1a5a6ef7d972dbc470417146dfdcbc7ff2b283c0
Monchoraptor <amoracho98@gmail.com>
2020-07-14 15:49:18 +0200

    fix itoa 12?

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,36 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                                    :+:      :+:    :+:   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amoracho <amoracho@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/21 21:15:26 by amoracho          #+#    #+#             */
-/*   Updated: 2020/07/14 00:43:30 by amoracho                   ###   ########.fr       */
+/*   Updated: 2020/07/14 00:43:30 by amoracho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	ft_count(int n)
+static int	ft_count(long n)
 {
-	int	i;
+	int i;
 
 	i = 0;
-	if (n == 0)
-		i = 2;
 	if (n < 0)
 	{
 		n = n * -1;
 		i++;
 	}
-	while (n)
+	while (n > 0)
 	{
 		n = n / 10;
 		i++;
 	}
 	return (i);
-
 }
 
 char		*ft_itoa(int n)
@@ -59,10 +56,11 @@ char		*ft_itoa(int n)
 	}
 	return (s);
 }
-
-int main(int argc, char **argv)
-{
-	(void)argc;
-	printf("%s\n",ft_itoa(ft_atoi(argv[1])));
-}
-
+/*
+**	int main(int argc, char **argv)
+**	{
+**		(void)argc;
+**		printf("%s\n",ft_itoa(ft_atoi(argv[1])));
+**	//	printf("%i",ft_count(-2147483648));
+**	}
+*/
